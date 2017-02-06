61bfc1ea18d2f25af5dfd82c8116a5234634b435
lcconly <lcconlycs@gmail.com>
2017-02-06 11:32:41 -0800

    first commit

---
+++ source.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding=utf-8
+#*************************************************************************
+#	> File Name: KNN.py
+#	> Author: liucheng
+#	> Mail: lcconlycs@gmail.com 
+#       > Created Time: Mon 06 Feb 2017 19:27:44 PM PST
+#*************************************************************************
+
